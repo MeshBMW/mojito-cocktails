46b9d8048b182f1d876698bf49979bba53c8b111
User Name <email@example.com>
2026-07-23 17:28:33 +0500

    implement navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
 import { navLinks } from '../../constants/index.js'
+import gsap from "gsap";
+import { useGSAP} from "@gsap/react";
+
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    })
+    navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+      backgroundColor: '#00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+      }
+    )
+  });
   return (
     <nav>
       <div>
